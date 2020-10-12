d467f0d64f27ff7d63b8b21d55d601c19b1a4693
Restoring73 <55996241+Restoring73@users.noreply.github.com>
2020-10-12 14:30:21 +0800

    Add files via upload

---
+++ lab4/lab4_part2/lab4.js
@@ -0,0 +1,16 @@
+function print_elements(node,txt, layer) {
+  var item=node.nodeName.toLowerCase();
+  if(item!="#text"){
+  	txt=txt+layer+item+"\n";
+  }
+  layer+="-";
+  for (var i = 0; i < node.childNodes.length; i++) {
+    var child = node.childNodes[i];
+    txt=print_elements(child,txt,layer);
+  }
+  return txt;
+}
+
+function aler(f){
+  alert(f.nodeName.toLowerCase());
+}
