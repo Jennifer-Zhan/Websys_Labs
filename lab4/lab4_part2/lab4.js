71eaffcc1ded5a333c2c2b41e1aa416b3139ff7e
JensenChen <a1043904820@gmail.com>
2020-10-13 17:09:23 +0800

    udpate the file and update part1b js

--- lab4/lab4_part2/lab4.js
+++ lab4/lab4_part2/lab4.js
@@ -1,19 +1,29 @@
-function print_elements(node,txt, layer) {
-  var item=node.nodeName.toLowerCase();
-  if(item!="#text"){
-  	txt=txt+layer+item+"\n";
-  }
-  layer+="-";
-  for (var i = 0; i < node.childNodes.length; i++) {
-    var child = node.childNodes[i];
-    node.childNodes[i].addEventListener("click", function () {
+function print_elements(node,txt,layer) {
+    var item=node.nodeName.toLowerCase();
+    if(item!="#text"){
+        txt=txt+layer+item+"\n";
+    }
+    layer+="-";
+    for (var i = 0; i < node.childNodes.length; i++) {
+        var child = node.childNodes[i];
+        node.childNodes[i].addEventListener("click", function () {
             alert(this.nodeName.toLowerCase());
         }, false);
-    txt=print_elements(child,txt,layer);
-  }
-  return txt;
+        txt=print_elements(child,txt,layer);
+    }
+    return txt;
 }
 
-function aler(f){
-  alert(f.nodeName.toLowerCase());
+function print_elements_b(element,txt,layer){
+    var item=element.tagName.toLowerCase();
+    txt=txt+layer+item+"\n";
+    layer+="-";
+    for (var i = 0; i < element.children.length; i++) {
+        var child = element.children[i];
+        element.children[i].addEventListener("click", function () {
+            alert(this.tagName.toLowerCase());
+        }, false);
+        txt=print_elements(child,txt,layer);
+    }
+    return txt;
 }
