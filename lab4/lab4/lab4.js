479f6f2ceae3bd34a17d3076f28ba5dc9374bfc5
Restoring73 <55996241+Restoring73@users.noreply.github.com>
2020-10-13 18:28:59 +0800

    Update lab4.js

--- lab4/lab4/lab4.js
+++ lab4/lab4/lab4.js
@@ -1,9 +1,9 @@
-function print_elements(node,txt,layer) {
-  var item=node.nodeName.toLowerCase();
-  if(item!="#text"){
-  	txt=txt+layer+item+"\n";
+function print_elements(node, txt, layer) {
+  var item = node.nodeName.toLowerCase();
+  if ( item != "#text" ) {
+  	txt = txt + layer + item + "\n";
   }
-  layer+="-";
+  layer += "-";
   for (var i = 0; i < node.childNodes.length; i++) {
     var child = node.childNodes[i];
     // add part2 - modify the function for part1a and 
