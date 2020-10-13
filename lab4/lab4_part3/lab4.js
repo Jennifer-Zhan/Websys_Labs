a8e453b54f47ba0986746110a53c280199e0e90c
JensenChen <a1043904820@gmail.com>
2020-10-13 11:20:08 +0800

    enable the elements move to the right still working on the part2

--- lab4/lab4_part3/lab4.js
+++ lab4/lab4_part3/lab4.js
@@ -6,22 +6,22 @@ function print_elements(node,txt, layer) {
   layer+="-";
   for (var i = 0; i < node.childNodes.length; i++) {
     var child = node.childNodes[i];
-    node.childNodes[i].addEventListener("click", function () {
-            alert(this.nodeName.toLowerCase());
-        }, false);
+    // node.childNodes[i].addEventListener("click", function () {
+    //         alert(this.nodeName.toLowerCase());
+    //     }, false);
     txt=print_elements(child,txt,layer);
   }
   return txt;
 }
 
 function mouseOver() {
-  this.style.backgroundColor = "blue";
-  this.style.right=this.style.right-"10px";
+  this.style.backgroundColor = "#1DA1F2";
+    this.style.marginLeft = "10px";
 }
 
 function mouseOut() {
   this.style.backgroundColor = "white";
-  this.style.right=this.style.right+"10px";
+  this.style.marginLeft = "0px";
 }
 
 
