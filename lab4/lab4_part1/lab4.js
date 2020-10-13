7457dce4a7dad5d4584f455cc1507d0088bfe8de
JensenChen <a1043904820@gmail.com>
2020-10-13 10:44:36 +0800

    update the placements in part1

--- lab4/lab4_part1/lab4.js
+++ lab4/lab4_part1/lab4.js
@@ -1,12 +1,12 @@
-function print_elements(node,txt, layer) {
-  var item=node.nodeName.toLowerCase();
-  if(item!="#text"){
-  	txt=txt+layer+item+"\n";
+function print_elements(node, txt, layer) {
+  var item = node.nodeName.toLowerCase();
+  if(item != "#text"){
+  	txt = txt+layer+item+"\n";
   }
   layer+="-";
-  for (var i = 0; i < node.childNodes.length; i++) {  
+  for (var i = 0; i < node.childNodes.length; i++) {
     var child = node.childNodes[i];
-    txt=print_elements(child,txt,layer);
+    txt = print_elements(child, txt, layer);
   }  
   return txt;
 }
