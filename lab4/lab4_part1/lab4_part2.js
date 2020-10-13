b1dc3a8333dfbd707726fbb7942f4bd60f34ed9f
JensenChen <a1043904820@gmail.com>
2020-10-13 14:54:56 +0800

    update the css, but the js in part3 not working now

---
+++ lab4/lab4_part1/lab4_part2.js
@@ -0,0 +1,12 @@
+function print_elements(node, txt, layer) {
+    var item = node.nodeName.toLowerCase();
+    if(item != "#text"){
+        txt = txt+layer+item+"\n";
+    }
+    layer+="-";
+    for (var i = 0; i < node.childNodes.length; i++) {
+        var child = node.childNodes[i];
+        txt = print_elements(child, txt, layer);
+    }
+    return txt;
+}
