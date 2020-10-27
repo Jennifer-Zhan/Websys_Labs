b4d405b3c13de85d87bdcc4944c724e1408c91da
Restoring73 <55996241+Restoring73@users.noreply.github.com>
2020-10-27 19:50:42 +0800

    Update lab5&6.js

--- lab5&6/lab5&6.js
+++ lab5&6/lab5&6.js
@@ -65,7 +65,7 @@ function new_game(turns){
     //document.getElementById("set_name").innerHTML=settings.name;
     document.getElementById("set_turns").innerHTML=turns;
   })
-  
+
   $("#new").click(function(){
     var correct=swatch_generate();
     $( "#red" ).slider( "value", (20));
@@ -91,12 +91,29 @@ function submit_guess(turns,correct){
     var green_percentoff = Math.round((Math.abs(correct[1]-$("#input2").val())/255)*100);
     var blue_percentoff = Math.round((Math.abs(correct[2]-$("#input3").val())/255)*100);
     var output = "Red: ";
-    output+=red_percentoff;
-    output+="%\nGreen: ";
-    output+=green_percentoff;
-    output+="%\nBlue: ";
-    output+=blue_percentoff;
-    output+="%";
+    if (red_percentoff == 0){
+      output+="You got it! (0% off)";
+    }
+    else{
+      output+=red_percentoff;
+      output+="%";
+    }
+    output+="\nGreen: ";
+    if (green_percentoff == 0){
+      output+="You got it! (0% off)";
+    }
+    else{
+      output+=green_percentoff;
+      output+="%";
+    }
+    output+="\nBlue: ";
+    if (blue_percentoff == 0){
+      output+="You got it! (0% off)";
+    }
+    else{
+      output+=blue_percentoff;
+      output+="%";
+    }
     document.getElementById("result").innerHTML=output;
     if(20000-interval<0){
       var tmp_scores=(300-(red_percentoff+green_percentoff+blue_percentoff))*0;
@@ -113,7 +130,7 @@ function submit_guess(turns,correct){
       document.getElementById("tmp_scores").innerHTML=tmp_scores;
     }
 
-    
+
     if(count==turns){
       max_scores=0;
       $("#setting_block").css("visibility", "visible");
@@ -128,14 +145,14 @@ function submit_guess(turns,correct){
       $('.timer').text("00:00:00");
       clearTimeout(interval);
       $( "#submit" ).replaceWith( "<button id='new'>New Game</button>" );
-      
+
       $("#setting_change").dialog();
       new_game(turns);
     }
   });
 }
 
-//when click on 
+//when click on
 function changeSetting(settings){
   $("#setting_block").append("<div id='setting_change'><input id='name'></input><div>");
   $('#name').attr("value",settings.name);
@@ -146,10 +163,10 @@ function pretty_time_string(num) {
 }
 
 function timer(){
-  var start = new Date;    
+  var start = new Date;
 
   interval=setInterval(function() {
-    var total_seconds = (new Date - start) / 1000;   
+    var total_seconds = (new Date - start) / 1000;
 
     var hours = Math.floor(total_seconds / 3600);
     total_seconds = total_seconds % 3600;
@@ -206,7 +223,7 @@ $.fn.hexed = function(settings) {
   this.append("<p id='best_scores'></p>");
   this.append("<p id='tmp_scores'></p>");
   this.append("<div id='setting_block'></div>");
-  
+
   var set = document.createElement( 'button' );
   $(set).attr("id", "set").attr("type","button").html("change setting");
   $("#setting_block").append(set);
@@ -223,6 +240,5 @@ $.fn.hexed = function(settings) {
 $(function() {
   var settings={"name":"Mike","turns":3};
   $("body").hexed(settings);
-  
+
 });
-  
