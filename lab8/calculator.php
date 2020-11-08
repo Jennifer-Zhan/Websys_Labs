6c908705d3d94911b33290b9cce1d221cce30a4c
Restoring73 <55996241+Restoring73@users.noreply.github.com>
2020-11-08 11:22:40 +0800

    Add files via upload

---
+++ lab8/calculator.php
@@ -0,0 +1,319 @@
+<?php
+
+abstract class Operation {
+  protected $operand_1;
+  protected $operand_2;
+  public function __construct($o1, $o2) {
+    // Make sure we're working with numbers...
+    if (!is_numeric($o1) || !is_numeric($o2)) {
+      throw new Exception('Non-numeric operand.');
+    }
+
+    // Assign passed values to member variables
+    $this->operand_1 = $o1;
+    $this->operand_2 = $o2;
+  }
+  public abstract function operate();
+  public abstract function getEquation();
+}
+
+// Addition subclass inherits from Operation
+class Addition extends Operation {
+  public function operate() {
+    return $this->operand_1 + $this->operand_2;
+  }
+  public function getEquation() {
+    return $this->operand_1 . ' + ' . $this->operand_2 . ' = ' . $this->operate();
+  }
+}
+
+class Subtract extends Operation {
+  public function operate() {
+    return $this->operand_1 - $this->operand_2;
+  }
+  public function getEquation() {
+    return $this->operand_1 . ' - ' . $this->operand_2 . ' = ' . $this->operate();
+  }
+}
+
+class Multiply extends Operation {
+  public function operate() {
+    return $this->operand_1 * $this->operand_2;
+  }
+  public function getEquation() {
+    return $this->operand_1 . ' * ' . $this->operand_2 . ' = ' . $this->operate();
+  }
+}
+
+class Divide extends Operation {
+  public function operate() {
+    return $this->operand_1 / $this->operand_2;
+  }
+  public function getEquation() {
+    return $this->operand_1 . ' / ' . $this->operand_2 . ' = ' . $this->operate();
+  }
+}
+
+class Square_root extends Operation {
+  public function operate() {
+    return sqrt($this->operand_1);
+  }
+  public function getEquation() {
+    return ' sqrt( ' . $this->operand_1 . ' ) ' . ' = ' . $this->operate();
+  }
+}
+
+class Square extends Operation {
+  public function operate() {
+    return $this->operand_1 * $this->operand_1;
+  }
+  public function getEquation() {
+    return $this->operand_1 . ' ^ ' . ' 2 ' . ' = ' . $this->operate();
+  }
+}
+
+class xpy extends Operation {
+  public function operate() {
+    if($this->operand_2 == 0){
+      return 1;
+    }
+    else if($this->operand_2 > 0){
+      $a = $this->operand_1;
+      for($x = 1; $x < $this->operand_2; $x++){
+        $a = $a * $this->operand_1;
+      }
+      return $a;
+    }
+    else{
+      $a = $this->operand_1;
+      for($x = -1; $x > $this->operand_2; $x--){
+        $a = $a * $this->operand_1;
+      }
+      return 1/$a;
+    }
+  }
+  public function getEquation() {
+    return $this->operand_1 . ' ^ ' . $this->operand_2 . ' = ' . $this->operate();
+  }
+}
+
+class tp extends Operation {
+  public function operate() {
+    if($this->operand_1 == 0){
+      return 1;
+    }
+    else if($this->operand_1 > 0){
+      $a = 10;
+      for($x = 1; $x < $this->operand_1; $x++){
+        $a = $a * 10;
+      }
+      return $a;
+    }
+    else{
+      $a = 10;
+      for($x = -1; $x > $this->operand_1; $x--){
+        $a = $a * 10;
+      }
+      return 1/$a;
+    }
+  }
+  public function getEquation() {
+    return ' 10 ' . ' ^ ' . $this->operand_1 . ' = ' . $this->operate();
+  }
+}
+
+class ep extends Operation {
+  public function operate() {
+    if($this->operand_1 == 0){
+      return 1;
+    }
+    else if($this->operand_1 > 0){
+      $a = 2.71828182845904523536;
+      for($x = 1; $x < $this->operand_1; $x++){
+        $a = $a * 2.71828182845904523536;
+      }
+      return $a;
+    }
+    else{
+      $a = 2.71828182845904523536;
+      for($x = -1; $x > $this->operand_1; $x--){
+        $a = $a * 2.71828182845904523536;
+      }
+      return 1/$a;
+    }
+  }
+  public function getEquation() {
+    return ' e ' . ' ^ ' . $this->operand_1 . ' = ' . $this->operate();
+  }
+}
+
+class sin extends Operation {
+  public function operate() {
+    return sin($this->operand_1);
+  }
+  public function getEquation() {
+    return ' sin( ' . $this->operand_1 . ' ) ' . ' = ' . $this->operate();
+  }
+}
+
+class cos extends Operation {
+  public function operate() {
+    return cos($this->operand_1);
+  }
+  public function getEquation() {
+    return ' cos( ' . $this->operand_1 . ' ) ' . ' = ' . $this->operate();
+  }
+}
+
+class tan extends Operation {
+  public function operate() {
+    return tan($this->operand_1);
+  }
+  public function getEquation() {
+    return ' tan( ' . $this->operand_1 . ' ) ' . ' = ' . $this->operate();
+  }
+}
+
+class ln extends Operation {
+  public function operate() {
+    return log($this->operand_1);
+  }
+  public function getEquation() {
+    return ' ln( ' . $this->operand_1 . ' ) ' . ' = ' . $this->operate();
+  }
+}
+
+class log extends Operation {
+  public function operate() {
+    return log($this->operand_1,10);
+  }
+  public function getEquation() {
+    return ' log( ' . $this->operand_1 . ' ) ' . ' = ' . $this->operate();
+  }
+}
+
+// Add subclasses for Subtraction, Multiplication and Division here
+
+
+// Some debugs - uncomment these to see what is happening...
+// echo '$_POST print_r=>',print_r($_POST);
+// echo "<br>",'$_POST vardump=>',var_dump($_POST);
+// echo '<br/>$_POST is ', (isset($_POST) ? 'set' : 'NOT set'), "<br/>";
+// echo "<br/>---";
+
+
+// Check to make sure that POST was received
+// upon initial load, the page will be sent back via the initial GET at which time
+// the $_POST array will not have values - trying to access it will give undefined message
+
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $o1 = $_POST['op1'];
+    $o2 = $_POST['op2'];
+  }
+  $err = Array();
+
+
+// Instantiate an object for each operation based on the values returned on the form
+// For example, check to make sure that $_POST is set and then check its value and
+// instantiate its object
+//
+// The Add is done below.  Go ahead and finish the remiannig functions.
+// Then tell me if there is a way to do this without the ifs
+// We might cover such a way on Tuesday...
+
+  try {
+    if (isset($_POST['add']) && $_POST['add'] == 'Add') {
+      $op = new Addition($o1, $o2);
+    }
+    if (isset($_POST['sub']) && $_POST['sub'] == 'Subtract') {
+      $op = new Subtract($o1, $o2);
+    }
+    if (isset($_POST['mult']) && $_POST['mult'] == 'Multiply') {
+      $op = new Multiply($o1, $o2);
+    }
+    if (isset($_POST['divi']) && $_POST['divi'] == 'Divide') {
+      $op = new Divide($o1, $o2);
+    }
+    if (isset($_POST['sqrt']) && $_POST['sqrt'] == 'Square_root') {
+      $op = new Square_root($o1, 0);
+    }
+    if (isset($_POST['power']) && $_POST['power'] == 'x^y') {
+      $op = new xpy($o1, $o2);
+    }
+    if (isset($_POST['sqr']) && $_POST['sqr'] == 'x^2') {
+      $op = new Square($o1, 0);
+    }
+    if (isset($_POST['tenPower']) && $_POST['tenPower'] == '10^x') {
+      $op = new tp($o1, 0);
+    }
+    if (isset($_POST['ePower']) && $_POST['ePower'] == 'e^x') {
+      $op = new ep($o1, 0);
+    }
+    if (isset($_POST['sin']) && $_POST['sin'] == 'Sin(x)') {
+      $op = new sin($o1, 0);
+    }
+    if (isset($_POST['cos']) && $_POST['cos'] == 'Cos(x)') {
+      $op = new cos($o1, 0);
+    }
+    if (isset($_POST['tan']) && $_POST['tan'] == 'Tan(x)') {
+      $op = new tan($o1, 0);
+    }
+    if (isset($_POST['ln']) && $_POST['ln'] == 'ln') {
+      $op = new ln($o1, 0);
+    }
+    if (isset($_POST['log']) && $_POST['log'] == 'log(10)') {
+      $op = new log($o1, 0);
+    }
+// Put code for subtraction, multiplication, and division here
+
+
+  }
+  catch (Exception $e) {
+    $err[] = $e->getMessage();
+  }
+?>
+
+<!doctype html>
+<html>
+<head>
+<title>PHP Calculator</title>
+</head>
+<body>
+  <pre id="result">
+  <?php
+    if (isset($op)) {
+      try {
+        echo $op->getEquation();
+      }
+      catch (Exception $e) {
+        $err[] = $e->getMessage();
+      }
+    }
+
+    foreach($err as $error) {
+        echo $error . "\n";
+    }
+  ?>
+  </pre>
+  <form method="post" action="calculator.php">
+    <input type="text" name="op1" id="name" value="" />
+    <input type="text" name="op2" id="name" value="" />
+    <br/>
+    <!-- Only one of these will be set with their respective value at a time -->
+    <input type="submit" name="add" value="Add" />
+    <input type="submit" name="sub" value="Subtract" />
+    <input type="submit" name="mult" value="Multiply" />
+    <input type="submit" name="divi" value="Divide" />
+    <input type="submit" name="sqrt" value="Square_root"/>
+    <input type="submit" name="sqr" value="x^2"/>
+    <input type="submit" name="power" value="x^y"/>
+    <input type="submit" name="tenPower" value="10^x"/>
+    <input type="submit" name="ePower" value="e^x"/>
+    <input type="submit" name="sin" value="Sin(x)"/>
+    <input type="submit" name="cos" value="Cos(x)"/>
+    <input type="submit" name="tan" value="Tan(x)"/>
+    <input type="submit" name="log" value="log(10)"/>
+    <input type="submit" name="ln" value="ln"/>
+  </form>
+</body>
+</html>
